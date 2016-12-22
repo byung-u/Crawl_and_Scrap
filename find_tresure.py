6b1f3087e96d295e470b526e220ac67ec9935b52
byung-u <iam.byungwoo@gmail.com>
2016-12-22 17:23:57 +0900

    Add search repo for all language
    Add all language logic.
    Fix by test.

--- find_tresure.py
+++ find_tresure.py
@@ -7,6 +7,8 @@ import sqlite3
 from twython import Twython
 from github import Github
 
+MAX_TWEET_MSG = 140
+
 
 class FTbot:  # Find Tresure 보물찾기 봇
     def __init__(self):
@@ -74,14 +76,16 @@ class UseGithub:
     def login(self):
         return Github(self.github_id, self.github_pw)
 
-    def get_check_options(self, past=2, stars=0):
+    def get_check_options(self, past, stars):
         check_date = datetime.datetime.now() - datetime.timedelta(days=past)
         date_range = '>=%s' % check_date.strftime('%Y-%m-%d')
         star_range = '>%s' % stars
         return date_range, star_range
 
-    def get_new_github_repo(self, search_lang='all', search_star=0):
-        date_range, star_range = self.get_check_options(stars=search_star)
+    def get_new_github_repo(self, search_lang=None, past=2, min_star=0):
+
+        date_range, star_range = self.get_check_options(past, stars=min_star)
+
         # https://developer.github.com/v3/search/#search-repositories
         # https://developer.github.com/v3/#rate-limiting
         repos = self.github_no_login.search_repositories(
@@ -90,46 +94,56 @@ class UseGithub:
                 stars=star_range,
                 sort="stars",
                 order="desc",
-                language=search_lang
+                language=search_lang,
         )
 
         send_msg = []
         s = UseSqlite3()
         for repo in repos:
-            print(repo)
+            lang = repo.get_languages()
             ret = s.already_sent(repo.html_url)
             if ret:
+                msg = 'Already sent %s' % repo.html_url
+                print(msg)
                 continue
             else:
                 # https://developer.github.com/v3/repos/
                 s.insert_url(repo.html_url)
-                if search_lang == 'all':
-                    msg = '[GITHUB(new) %s]\nstar:%s\nlang:%s\n\n%s\n%s' % (
-                            search_lang, repo.stargazers_count,
-                            repo.language,
-                            repo.html_url, repo.description)
-                else:
-                    msg = '[GITHUB(new) %s]\nstar:%s\n\n%s\n%s' % (
-                            search_lang, repo.stargazers_count,
-                            repo.html_url, repo.description)
-
-                if len(msg) > 140:
+                msg = '[GITHUB(new) %s]\nstar:%s\n\n%s\n%s' % (
+                        list(lang.keys())[0],
+                        repo.stargazers_count,
+                        repo.html_url,
+                        repo.description
+                )
+                if len(msg) > MAX_TWEET_MSG:
                     msg = '%s...' % msg[0:137]
 
                 send_msg.append(msg)
-
         s.close()
         return send_msg
+
+
+def post_tweet(ft, msg, msg_len):
+    for i in range(msg_len):
+        print(msg[i])
+        ft.post_tweet(msg[i])
 
 
 def main():
     g = UseGithub()
 
-    new_github_repo = g.get_new_github_repo('python', 0)
-    ft = FTbot()
-    for i in range(len(new_github_repo)):
-        print(new_github_repo[i])
-        ft.post_tweet(new_github_repo[i])
+    send_msg = []
+    msg = g.get_new_github_repo('python', 2, 0)
+    if len(msg) > 0:
+        send_msg.append(msg)
+
+    msg = g.get_new_github_repo(None, 2, 3)
+    if len(msg) > 0:
+        send_msg.append(msg)
+
+    if len(send_msg) > 0:
+        ft = FTbot()
+        post_tweet(ft, send_msg, len(send_msg))
 
 
 if __name__ == '__main__':
