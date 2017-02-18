63e328e12e4ecb04cf61df4070f73259c54e9cb1
byung-u <iam.byungwoo@gmail.com>
2017-02-19 00:56:21 +0900

    FIX sleep time

--- find_the_treasure.py
+++ find_the_treasure.py
@@ -92,7 +92,7 @@ class FTbot:  # Find the Treasure 보물찾기 봇
 
 def ft_post_tweet_array(ft, msg):
     for i in range(len(msg)):
-        time.sleep(2)
+        time.sleep(1)
         ft.post_tweet(msg[i])
 
 
@@ -207,6 +207,7 @@ def finding_about_realestate(ft):
         send_gmail(ft, 'Daum Blogs', daum_blog)
 
     sgx = get_rate_of_process_sgx(ft)
+    time.sleep(1)
     ft.post_tweet(sgx)
 
 
