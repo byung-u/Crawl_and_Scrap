2101209cd2b279edb2d050c3a82528d86b4da06b
byung-u <iam.byungwoo@gmail.com>
2017-03-07 11:50:01 +0900

    FIX Development Status 2 -> 5

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
 
         'Environment :: Console', 
         # Indicate who your project is intended for
