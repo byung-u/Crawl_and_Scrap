faac7a7e176f688c4dd3458c90643d81a4034b0c
byung-u <iam.byungwoo@gmail.com>
2017-08-11 14:53:59 +0900

    Add woowabros

--- find_the_treasure/ft_tech_blog.py
+++ find_the_treasure/ft_tech_blog.py
@@ -2,12 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
-from datetime import datetime
 from requests import get, codes
-from time import gmtime, strftime, time
 
-from find_the_treasure import defaults
 from find_the_treasure.ft_sqlite3 import UseSqlite3
+
 
 class TechBlog:
     def __init__(self, ft):
@@ -27,6 +25,31 @@ class TechBlog:
                 post = auth.find(ft.match_soup_class(['post-title']))
                 desc = auth.find(ft.match_soup_class(['post-description']))
                 result_url = '%s%s' % (base_url, post.a['href'][1:])
+                if self.sqlite3.already_sent_tech_blog(result_url):
+                    # already exist
+                    continue
+                self.sqlite3.insert_tech_blog(result_url)
+
+                result = '[Spoqa]\n%s\n%s' % (desc.string, result_url)
+                result = ft.check_max_tweet_msg(result)
+                send_msg.append(result)
+        return send_msg
+
+    def woowabros(self, ft):
+        send_msg = []
+        base_url = 'http://woowabros.github.io'
+        url = 'http://woowabros.github.io/index.html'
+        r = get(url)
+        if r.status_code != codes.ok:
+            ft.logger.error('[Tech blog woowabros] request error, code=%d', r.status_code)
+            return
+        soup = BeautifulSoup(r.text, 'html.parser')
+        for l in soup.find_all(ft.match_soup_class(['list'])):
+            for lm in l.find_all(ft.match_soup_class(['list-module'])):
+                desc = lm.find(ft.match_soup_class(['post-description']))
+                result_url = '[Woowabros]\n%s%s' % (base_url, lm.a['href'])
+                if result_url is None or desc.string is None:
+                    continue
                 if self.sqlite3.already_sent_tech_blog(result_url):
                     # already exist
                     continue
