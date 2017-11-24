f5c3759a48e20b5bb178fac13f6a6b1774949516
byung-u <iam.byungwoo@gmail.com>
2017-11-24 11:30:29 +0900

    Fix omitting message

--- brute_web_crawler/main.py
+++ brute_web_crawler/main.py
@@ -155,7 +155,7 @@ class BW:  # Brute Web crawler
         if msg_len > defaults.MAX_TWEET_MSG:
             over_len = msg_len - defaults.MAX_TWEET_MSG + 3 + 2  # ... + margin
             msg_encode = msg_encode[0:(msg_len - over_len)]
-            msg = '%s' % msg_encode.decode("utf-8", "ignore")
+            msg = '%s...' % msg_encode.decode("utf-8", "ignore")
             self.logger.info('[Over 140 omitting]%s', msg)
         return msg
 
