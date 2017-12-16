e8e7ccbbbcd612f0ebf7f9072bd45983a49e136a
byung-u <iam.byungwoo@gmail.com>
2017-12-17 08:21:13 +0900

    Add finlife

--- blogging/dexa.py
+++ blogging/dexa.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import json
 import os
 import re
 
@@ -63,6 +64,7 @@ def get_tistory_token():
     print(redirect_url)
     temp = re.split('access_token=', redirect_url)
     token = re.split('&state=', temp[1])[0]
+    driver.quit()
     return token
 
 
@@ -230,11 +232,101 @@ def vic_market():
     return result
 
 
+def join_deny(num):
+    if num == '1':
+        return '가입 제한없음'
+    elif num == '2':
+        return '서민전용 가입'
+    elif num == '3':
+        return '가입 일부제한'
+    else:
+        return 'Unknown'
+
+
+def dcls_end_day(day):  # 공시종료일
+    if day is None:
+        return '공시 종료일 미정'
+    else:
+        return '공시 종료일 ' + day
+
+
+def max_limit(money):  # 공시종료일
+    if money is None:
+        return '한도정보 없음'
+    else:
+        m = str(money)
+        ret = m.endswith('00000000')
+        if ret is True:
+            return m[0:-8] + '억원'
+
+        ret = m.endswith('0000000')
+        if ret is True:
+            return m[0:-7] + '천만원'
+        else:
+            return m + '원'
+
+def fixed_deposit():
+
+    bank_code = {
+            '우리은행': '0010001',
+            '한국스탠다드차타드은행': '0010002',
+            '한국씨티은행': '0010006',
+            '대구은행': '0010016',
+            '부산은행': '0010017',
+            '광주은행': '0010019',
+            '제주은행': '0010020',
+            '전북은행': '0010022',
+            '경남은행': '0010024',
+            '중소기업은행': '0010026',
+            '한국산업은행': '0010030',
+            '국민은행': '0010927',
+            '신한은행': '0011625',
+            '농협은행': '0013175',
+            'KEBKEB하나은행': '0013909',
+            '케이뱅크은행': '0014674',
+            '수협은행': '0014807',
+            '한국카카오은행': '0015130',
+            }
+    result = '금융감독원의 금융상품통합 비교공시 정보를 바탕으로 작성된 글입니다.<br><br><br>'
+    auth_key = os.environ.get('FINLIFE_KEY')
+
+    for bank, code in bank_code.items():
+        url = 'http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth=%s&topFinGrpNo=020000&pageNo=1&financeCd=%s' % (auth_key, code)
+        r = get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        js = json.loads(str(soup))
+        result = '%s<h3>%s</h3><br><table border="1" cellspacing="0" cellpadding="3" bordercolor="#  999999" style="border-collapse:collapse">' % (result, bank)
+        result = '''%s<tr>
+        <th>금융상품,가입방법</th>
+        <th>우대조건</th>
+        <th>가입대상</th>
+        <th>최고한도</th>
+        </tr>''' % (result)
+        for banks in js['result']['baseList']:
+            # print(banks['fin_prdt_nm'])
+            result = '%s<tr>' % result
+            result = '%s<td><font color="red">%s</font><br><br>➡ %s으로 가입<br>➡ %s<br>➡ %s</td>' % (result, banks["fin_prdt_nm"], banks['join_way'], join_deny(banks['join_deny']), dcls_end_day(banks['dcls_end_day']))
+            # result = '%s<td>%s</td>' % (result, banks["join_way"])
+            result = '%s<td>%s</td>' % (result, banks["spcl_cnd"].replace('\n', '<br>'))
+            # result = '%s<td>%s</td>' % (result, banks['join_member'])
+            result = '%s<td>%s</td>' % (result, banks['etc_note'].replace('\n', '<br>'))
+            result = '%s<td>%s</td>' % (result, max_limit(banks['max_limit']))
+            result = '%s</tr>' % result
+        result = '%s</table><br><br><br>' % result
+    return result
+
+
 def main():
     now = datetime.now()
     cur_time = '%4d%02d%02d' % (now.year, now.month, now.day)
 
     token = get_tistory_token()
+
+    title = '[%s] 정기예금 금리 정보' % cur_time
+    content = fixed_deposit()
+    tistory_post(token, title, content, '731649')
+
+    return
 
     title = '[%s] 빅마켓 지점별 휴관일, 영업시간, 주소, 연락처 정보' % cur_time
     content = vic_market()
